72fc7d5cb738bf9947cd0e639be78963f7b2dd01
Alice <agenkin1@gmail.com>
2020-06-18 14:49:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ elsif array.length == 3
   new_last_array_item = "and #{array[-1]}"
   array.pop
   first_half = array.join(",")
-  newArray = array.push(new_last_array_item)
+  newArray = first_half + array.push(new_last_array_item)
 else array.join(",")
 end
 end
