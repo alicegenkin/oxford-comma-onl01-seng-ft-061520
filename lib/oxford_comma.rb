267abf26d0f429336da73c023a29cb2383be818f
Alice <agenkin1@gmail.com>
2020-06-18 14:39:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,11 @@
 def oxford_comma(array)
 if array.length == 2
   array.join(" and ")
+elsif array.length == 3
+  array[0,1].join(,)
+  array[2].join(" and ")
+  
+  
 else array.join(",")
 end
 end
